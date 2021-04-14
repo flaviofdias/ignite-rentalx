2b40e1add9b90ae77adca97ba578b08285da10a0
Flavio Dias <flaviodias.info@gmail.com>
2021-04-13 22:16:13 -0300

    Feature Upload File

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -3,21 +3,59 @@ import fs from "fs";
 
 import { ICategoriesRepository } from "../../repositories/ICategoriesRepository";
 
+interface IImportCategory {
+  name: string;
+  description: string;
+}
+
 class ImportCategoryUseCase {
   constructor(private categoriesRepository: ICategoriesRepository) {}
 
-  execute(file: Express.Multer.File): void {
-    const stream = fs.createReadStream(file.path);
+  loadCategories(file: Express.Multer.File): Promise<IImportCategory[]> {
+    return new Promise((resolve, reject) => {
+      const stream = fs.createReadStream(file.path);
+      const categories: IImportCategory[] = [];
 
-    const parseFile = csvParse({
-      delimiter: ",",
-    }); // Por padrão entende que a vírgula (,) é o delimitador
+      const parseFile = csvParse({
+        delimiter: ",",
+      }); // Por padrão entende que a vírgula (,) é o delimitador
 
-    stream.pipe(parseFile); // Pega o pedaço lido e envia para o parseFile
+      stream.pipe(parseFile); // Pega o pedaço lido e envia para o parseFile
 
-    parseFile.on("data", async (line) => {
-      console.log(line);
+      parseFile
+        .on("data", async (line) => {
+          const [name, description] = line;
+          categories.push({
+            name,
+            description,
+          });
+        })
+        .on("end", () => {
+          resolve(categories);
+        })
+        .on("error", (err) => {
+          reject(err);
+        });
     });
+  }
+
+  async execute(file: Express.Multer.File): Promise<void> {
+    const categories = await this.loadCategories(file);
+
+    categories.map(async (category) => {
+      const { name, description } = category;
+
+      const existCategory = this.categoriesRepository.findByName(name);
+
+      if (!existCategory) {
+        this.categoriesRepository.create({
+          name,
+          description,
+        });
+      }
+    });
+
+    // console.log(categories);
   }
 }
 
