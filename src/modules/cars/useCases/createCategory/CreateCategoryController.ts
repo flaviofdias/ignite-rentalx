3c6b46bfdad9580d71825914922650a687578350
Flavio Dias <flaviodias.info@gmail.com>
2021-04-24 04:07:54 -0300

    Working Database

--- src/modules/cars/useCases/createCategory/CreateCategoryController.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryController.ts
@@ -5,10 +5,10 @@ import { CreateCategoryUseCase } from "./CreateCategoryUseCase";
 class CreateCategoryController {
   constructor(private createCateoryUseCase: CreateCategoryUseCase) {}
 
-  handle(request: Request, response: Response): Response {
+  async handle(request: Request, response: Response): Promise<Response> {
     const { name, description } = request.body;
 
-    this.createCateoryUseCase.execute({ name, description });
+    await this.createCateoryUseCase.execute({ name, description });
 
     return response.status(201).send();
   }
