5aa82d14c7a6bcab01b8f268b38f3582f47a2079
Flavio Dias <flaviodias.info@gmail.com>
2021-05-09 11:46:19 -0300

    ALTER: Notation @Import

--- src/modules/cars/repositories/implementations/SpecificationsRepository.ts
+++ src/modules/cars/repositories/implementations/SpecificationsRepository.ts
@@ -2,8 +2,8 @@ import { getRepository, Repository } from "typeorm";
 
 import { Specification } from "../../entities/Specification";
 import {
-  ISpecificationRepository,
   ICreateSpecificationDTO,
+  ISpecificationRepository,
 } from "../ISpecificationsRepository";
 
 class SpecificationRepository implements ISpecificationRepository {
